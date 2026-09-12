8229d10728910747dca97b93380b323bb5f4388a
Ollcreeper12 <greeny11111111111111111@gmail.com>
2026-09-12 09:44:58 +0100

    ,n

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,12 +1,15 @@
 import {Circle, makeScene2D} from '@motion-canvas/2d';
 import {createRef} from '@motion-canvas/core';
+import {ColorPicker} from "../comp/ColorPicker";
 
 export default makeScene2D(function* (view) {
-  // Create your animations here
 
-  const circle = createRef<Circle>();
 
-  view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
 
-  yield* circle().scale(2, 2).to(1, 2);
+    view.add(
+        <ColorPicker
+            ref={}
+        />
+    )
+
 });
